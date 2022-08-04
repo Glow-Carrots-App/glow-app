82664b77f54e194f08e9abdb80b448774d631fb2
Gino Ochoa <gochoa1088@gmail.com>
2022-08-04 12:39:24 -0700

    Gino develop

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Glow Gino Vincent Robby!</Text>
+      <Text>Gino Develop</Text>
       <StatusBar style="auto" />
     </View>
   );
