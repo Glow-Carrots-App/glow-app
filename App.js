eb49c3f447d378bbe8720968dd85fcaf776a459c
Gino Ochoa <gochoa1088@gmail.com>
2022-08-04 12:03:03 -0700

    Gino connected

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Glow</Text>
+      <Text>Glow Gino</Text>
       <StatusBar style="auto" />
     </View>
   );
